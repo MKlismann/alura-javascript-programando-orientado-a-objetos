e88997e68a7aa37db5f5dd150834550f2ee87963
MKlismann <MKlismann@users.noreply.github.com>
2020-12-17 20:22:14 -0300

    Trabalhando com atributos estáticos

--- index.js
+++ index.js
@@ -10,6 +10,8 @@
             - Null (aplica NADA ao espaço de memória; intencional) e Undefined (recurso não definido previamente, por acaso)
             - Getters e Setters
             - instanceof
+            - Construtores
+            - Atributos estáticos
         - OBS: Imagine o seguinte cenário:
             - Criaremos um sistema de contas correntes e cadastro de clientes para o cliente 'ByteBank'.
             - O sistema deve permitir a manipulação do saldo dos usuários e operações de transferências bancárias.
@@ -18,33 +20,26 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Fulano";
-cliente1.cpf = 11122233344;
-cliente1.rg = 12345678;
+const cliente1 = new Cliente("Fulano", 11122233344);
 //cliente1.qualquerCoisa = "Qualquer coisa"; //JavaScript é dinamicamente tipado; Isto adiciona um novo atributo à classe (mesmo que não definido antes - lembre-se do CaseSensitive).
 console.log(cliente1);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Cicrano";
-cliente2.cpf = 22233344455;
+const cliente2 = new Cliente("Cicrano", 22233344455);
 console.log(cliente2);
 
-const contaCorrenteCliente1 = new ContaCorrente();
-contaCorrenteCliente1.agencia = 1001;
-contaCorrenteCliente1.cliente = cliente1;
+const contaCorrenteCliente1 = new ContaCorrente(1001, cliente1);
 contaCorrenteCliente1.depositar(500);
 console.log(contaCorrenteCliente1);
 
 const valorSacado = contaCorrenteCliente1.sacar(50);
 console.log(`valorSacado contaCorrenteCliente1: ${valorSacado}`);
 
-const contaCorrenteCliente2 = new ContaCorrente();
-contaCorrenteCliente2.agencia = 1002;
-contaCorrenteCliente2.cliente = cliente2;
+const contaCorrenteCliente2 = new ContaCorrente(1002, cliente2);
 
 console.log(contaCorrenteCliente1.saldo);
 console.log(contaCorrenteCliente2.saldo);
 contaCorrenteCliente1.transferir(100, contaCorrenteCliente2);
 console.log(contaCorrenteCliente1.saldo);
 console.log(contaCorrenteCliente2.saldo);
+
+console.log(ContaCorrente.numeroDeContas);
